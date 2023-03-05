b150347a11db23906ff23cfc3718dbea3127575a
Kisum <2313810210@qq.com>
2023-03-05 19:37:22 +0800

    Delete Mapper directory

--- Mapper/SysItemMapper.java
+++
@@ -1,9 +0,0 @@
-package com.server.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.server.Entity.SysItem;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysItemMapper extends BaseMapper<SysItem> {
-}
--- Mapper/SysUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.server.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.server.Entity.SysUser;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysUserMapper extends BaseMapper<SysUser> {
-}
