f0abd32b968551d92b009e4a2aea41caf30f7365
Kisum <2313810210@qq.com>
2023-03-05 19:56:09 +0800

    Add files via upload

---
+++ Mapper/SysItemMapper.java
@@ -0,0 +1,9 @@
+package com.server.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.server.Entity.SysItem;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SysItemMapper extends BaseMapper<SysItem> {
+}
