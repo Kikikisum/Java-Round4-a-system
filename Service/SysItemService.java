55eaa25b4026161eb9df83859b8f14cfe8a525ae
Kisum <2313810210@qq.com>
2023-03-05 19:37:32 +0800

    Delete Service directory

--- Service/SysItemService.java
+++
@@ -1,9 +0,0 @@
-package com.server.Service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.server.Entity.SysItem;
-
-public interface SysItemService extends IService<SysItem> {
-
-    public int  addOneItem(SysItem item);
-}
